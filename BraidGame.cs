9d1b049f5f44e0abd099c27297628f08d69480d1
Christian <wr00m@users.noreply.github.com>
2023-09-13 20:09:04 +0200

    Refactor everything

---
+++ BraidGame.cs
@@ -0,0 +1,53 @@
+﻿namespace BraidCam;
+
+using System.Diagnostics;
+
+class BraidGame : IDisposable
+{
+    public static bool TryGetRunningInstance(out BraidGame? braidGame)
+    {
+        var process = Process.GetProcessesByName("braid").FirstOrDefault();
+        braidGame = process != null ? new(process) : null;
+        return false;
+    }
+
+    private Process _process;
+    private ProcessMemoryHandler _processMemoryHandler;
+    public bool IsRunning => _process.HasExited == false;
+    public bool IsSteamVersion => _process.Modules[0].ModuleMemorySize == 7663616;
+
+    private BraidGame(Process process)
+    {
+        _process = process;
+        _processMemoryHandler = new(process.Id, process.Modules[0].BaseAddress);
+    }
+
+    public void Dispose()
+    {
+        _processMemoryHandler.Dispose();
+        _process.Dispose();
+    }
+
+    private static readonly byte[] _camEnabledBytes = new byte[] { 0xF3, 0x0F, 0x11 };
+    private static readonly byte[] _camDisabledBytes = new byte[] { 0x90, 0x90, 0x90 };
+
+    private const IntPtr _camUpdateXAddr = 0xA0367;
+    public bool CamLockX
+    {
+        get => _processMemoryHandler!.ReadBytes(_camUpdateXAddr, _camDisabledBytes.Length).SequenceEqual(_camDisabledBytes);
+        set => _processMemoryHandler!.WriteBytes(_camUpdateXAddr, value ? _camDisabledBytes : _camEnabledBytes);
+    }
+
+    private const IntPtr _camUpdateYAddr = 0xA036F;
+    public bool CamLockY
+    {
+        get => _processMemoryHandler!.ReadBytes(_camUpdateYAddr, _camDisabledBytes.Length).SequenceEqual(_camDisabledBytes);
+        set => _processMemoryHandler!.WriteBytes(_camUpdateYAddr, value ? _camDisabledBytes : _camEnabledBytes);
+    }
+
+    private const IntPtr camPosXAddr = 0x1F6ABC;
+    public float CamPosX { get => _processMemoryHandler!.ReadFloat(camPosXAddr); set => _processMemoryHandler!.WriteFloat(camPosXAddr, value); }
+
+    private const IntPtr camPosYAddr = camPosXAddr + sizeof(float);
+    public float CamPosY { get => _processMemoryHandler!.ReadFloat(camPosYAddr); set => _processMemoryHandler!.WriteFloat(camPosYAddr, value); }
+}
---
+++ ProcessMemoryHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace BraidCam;
+
+using System.Runtime.InteropServices;
+
+class ProcessMemoryHandler : IDisposable
+{
+    private IntPtr _hProc;
+    private IntPtr _baseAddress;
+
+    public ProcessMemoryHandler(int processId, IntPtr baseAddress)
+    {
+        _hProc = OpenProcess(ProcessAccessFlags.All, false, processId);
+        _baseAddress = baseAddress;
+    }
+
+    public void Dispose()
+    {
+        if (_hProc != default) { CloseHandle(_hProc); }
+    }
+
+    public float ReadFloat(IntPtr addr) => BitConverter.ToSingle(ReadBytes(addr, sizeof(float)), 0);
+    public void WriteFloat(IntPtr addr, float val) => WriteBytes(addr, BitConverter.GetBytes(val));
+    public byte[] ReadBytes(IntPtr addr, int count) { var buff = new byte[count]; ReadProcessMemory(_hProc, _baseAddress + addr, buff, count, out _); return buff; }
+    public void WriteBytes(IntPtr addr, byte[] bytes) => WriteProcessMemory(_hProc, _baseAddress + addr, bytes, (uint)bytes.Length, out var _);
+
+    [DllImport("kernel32.dll")] static extern IntPtr OpenProcess(ProcessAccessFlags dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
+    [DllImport("kernel32.dll", SetLastError = true)] static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead);
+    [DllImport("kernel32.dll", SetLastError = true)] static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+    [DllImport("kernel32.dll")] static extern int CloseHandle(IntPtr hProcess);
+    enum ProcessAccessFlags : uint { All = 0x001F0FFF }
+}
